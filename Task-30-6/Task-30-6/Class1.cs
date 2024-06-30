3fa75ad190361c9c70f1a95b3fb3d785d7809aff
ahmadqadomi-1 <qadomia95@gmail.com>
2024-07-01 02:06:56 +0300

    Task 30-6

---
+++ Task-30-6/Task-30-6/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_30_6
+{
+    internal class Class1
+    {
+        public void AvaSum(double[] arr01)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr01.Length; i++)
+            {
+                sum = sum + arr01[i];
+            }
+            double avarege = sum / arr01.Length;
+            Console.WriteLine(sum);
+            Console.WriteLine(avarege);
+        }
+
+        public void Cube(double cubenum)
+        {
+            double number01 = cubenum * cubenum * cubenum;
+            Console.WriteLine(number01);
+        }
+        public void Above1950s(int[] years)
+        {
+            for (int i = 0; i < years.Length; i++)
+            {
+                if (years[i] > 1950)
+                {
+                    Console.WriteLine(years[i]);
+                }
+            }
+        }
+        public int YearsToDay(int years)
+        {
+            int days = years * 365; 
+            return days;
+        }
+    }
+}
---
+++ Task-30-6/Task-30-6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_30_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 AvNum = new Class1();
+
+            //Task 1.
+
+            double[] numbers = { 1 , 2 , 3  , 4 , 5 , 6 , 7 , 8 , 9 , 10 }; 
+            AvNum.AvaSum(numbers);
+
+
+            //Task 2.
+
+            Console.Write(" Choose a number from 1 to 10 :  ");
+            double num1 = double.Parse (Console.ReadLine() );
+            for (int i = 1; i <= num1; i++)
+            {
+                AvNum.Cube(i);
+            }
+
+
+            //Task 3.
+
+            int[] arr03 = { 1763, 1972, 1925, 1916, 1984, 1124, 1950, 2020 };
+            AvNum.Above1950s(arr03);
+
+
+            //Task 4.
+
+            int Years = 24;
+            Console.WriteLine(AvNum.YearsToDay(Years));
+
+
+            //Task 5.
+
+            Informations detailsInformation = new Informations(24, "male", "Ahmad", "ahmad.alqadomi02@gmail.com", 15667, "0798453201");
+
+            detailsInformation.Display();
+        }
+    }
+}
